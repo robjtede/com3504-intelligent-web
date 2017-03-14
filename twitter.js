43197aa40d5ecc0e9a3880ea00d69e61f695fcfe
Ben Clegg <bclegg@midcode.co.uk>
2017-03-14 15:55:00 +0000

    added timeout

--- twitter.js
+++ twitter.js
@@ -5,11 +5,12 @@ var T = new Twit({
   consumer_key: keys.api_key,
   consumer_secret: keys.api_secret,
   access_token: keys.access_key,
-  access_token_secret: keys.access_secret
+  access_token_secret: keys.access_secret,
+  timeout_ms: 60 * 1000
 });
 
 module.exports.search = function (query, cb) {
-  T.get('search/tweets', {q: query},
+  T.get('search/tweets', {q: query, count: 500},
     function (err, data, response) {
       if (err) console.error(err);
       for (var index in data.statuses) {
