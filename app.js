db77992ad127073686d0f720576e612b9c1ae551
Ben Clegg <bclegg@midcode.co.uk>
2017-03-30 22:27:00 +0100

    io accessible in getTweets function

--- app.js
+++ app.js
@@ -9,20 +9,22 @@ app.set('view engine', 'pug');
 
 app.use(express.static('./public')); // Static file serving
 
-require('./routes/index')(app); // Routing
-
 // Socket.io setup
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
 // Start server
 const port = process.env.PORT || 3000;
 http.listen(port, function () {
   console.log('Started server on port: ' + port);
 });
 
+// Socket.io connection check
 io.on('connection', function (socket) {
   console.log('User connected.');
   socket.on('disconnect', function () {
     console.log('User disconnected.');
   });
 });
+
+require('./routes/index')(app, io); // Express routing
--- routes/index.js
+++ routes/index.js
@@ -3,38 +3,40 @@
 const twitter = require('../lib/twitter');
 const sql = require('../lib/sql');
 
-module.exports = app => {
-  app.get('/', getTweets);
+module.exports = (app, io) => {
+  app.get('/', getTweets(io));
 };
 
-const getTweets = function (req, res) {
-  const q = req.query;
-  // Search if there is at least one query
-  if (Object.getOwnPropertyNames(q).length !== 0) {
-    // First retrieve from local db, this will be the fastest
-    sql.getTweets(q, function (results) {
-      // console.log(results);
-      res.render('index', {
-        tweets: results,
-        qPlayer: q.player,
-        qTeam: q.team,
-        qAuthor: q.author
-      });
-    });
-
-    // Now retrieve more tweets from twitter, and add to page
-    twitter
-      .search(q)
-      .then(function (data) {
-        // TODO send to page with socket io
-        sql.insertTweetMulti(data); // Insert new tweets into database
+const getTweets = function (io) {
+  return function (req, res) {
+    const q = req.query;
+    // Search if there is at least one query
+    if (Object.getOwnPropertyNames(q).length !== 0) {
+      // First retrieve from local db, this will be the fastest
+      sql.getTweets(q, function (results) {
+        // console.log(results);
+        res.render('index', {
+          tweets: results,
+          qPlayer: q.player,
+          qTeam: q.team,
+          qAuthor: q.author
+        });
       });
 
-    // Now listen to stream, adding to page as received
-    // TODO stream api
-  } else {
+      // Now retrieve more tweets from twitter, and add to page
+      twitter
+        .search(q)
+        .then(function (data) {
+          // TODO send to page with socket io
+          sql.insertTweetMulti(data); // Insert new tweets into database
+        });
+
+      // Now listen to stream, adding to page as received
+      // TODO stream api
+    } else {
     // Render the page with no tweets
-    console.log('No queries submitted!');
-    res.render('index', {});
-  }
+      console.log('No queries submitted!');
+      res.render('index', {});
+    }
+  };
 };
