3a0307cb0e15c6545ab2cb656f83361b64613a3c
Ben Clegg <bclegg@midcode.co.uk>
2017-03-31 18:41:09 +0100

    sockets for cached tweets

--- routes/index.js
+++ routes/index.js
@@ -10,28 +10,31 @@ module.exports = (app, io) => {
 const getTweets = function (io) {
   return function (req, res) {
     const q = req.query;
+    // Render page
+    res.render('index', {
+      qPlayer: q.player,
+      qTeam: q.team,
+      qAuthor: q.author
+    });
+
     // Search if there is at least one query
     if (Object.getOwnPropertyNames(q).length !== 0) {
-      // First retrieve from local db, this will be the fastest
-      sql.getTweets(q, function (results) {
-        // console.log(results);
-        res.render('index', {
-          tweets: results,
-          qPlayer: q.player,
-          qTeam: q.team,
-          qAuthor: q.author
-        });
-      });
-
       // Socket connection
       io.on('connection', function (socket) {
         console.log('User connected.');
+
+        // First retrieve from local db, this will be the fastest
+        sql.getTweets(q, function (results) {
+          // console.log(results);
+          socket.emit('cachedTweets', results);
+        });
+
         // Now retrieve more tweets from twitter, and add to page
         twitter
           .search(q)
           .then(function (data) {
             // TODO remove tweets already in page
-            socket.emit('gettweets', data);
+            socket.emit('getRemoteTweets', data);
             sql.insertTweetMulti(data); // Insert new tweets into database
           });
 
