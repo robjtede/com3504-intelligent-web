963a1870f524411068a1626bec96d3684994902c
Rob Ede <robjtede@icloud.com>
2017-05-11 16:16:06 +0100

    add list page

--- routes/trackings.js
+++ routes/trackings.js
@@ -5,11 +5,16 @@ var sql = require('../lib/sql');
 module.exports = function (app) {
   app.get('/trackings/new', newTrack);
   app.get('/trackings/:id', show);
+  app.post('/trackings', list);
   app.get('/trackings', list);
 };
 
 function newTrack (req, res) {
   res.render('track/new');
+}
+
+function create (req, res) {
+
 }
 
 function list (req, res) {
@@ -20,21 +25,36 @@ function list (req, res) {
     function (err, results, fields) {
       if (err) throw new Error(err);
 
-      console.log(results);
-      res.render('track/list', {
-        trackings: results
-      });
+      db.query(
+        'SELECT * FROM searches ORDER BY id DESC',
+        function (err, results, fields) {
+          if (err) throw new Error(err);
+
+          res.render('track/list', {
+            trackings: results
+          });
+        }
+      );
     }
   );
 }
 
 function show (req, res) {
   var q = req.query;
+  var db = sql.getConnection();
 
-  // Render page
-  res.render('track/show', {
-    qPlayer: q.player,
-    qTeam: q.team,
-    qAuthor: q.author
-  });
+  db.query(
+    'SELECT * FROM searches ORDER BY id DESC LIMIT 10',
+    function (err, results, fields) {
+      if (err) throw new Error(err);
+
+        // Render page
+      res.render('track/show', {
+        player: q.player,
+        team: q.team,
+        author: q.author,
+        trackings: results
+      });
+    }
+  );
 };
