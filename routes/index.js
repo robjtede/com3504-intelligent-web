7e77fef6cc3a99299b03a0d774fc87d44659a606
Rob Ede <robjtede@icloud.com>
2017-05-11 11:41:37 +0100

    style nav

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,24 @@
 'use strict';
 
 module.exports = function (app) {
-  app.get('/', getTweets);
+  app.get('/', index);
+  app.get('/trackings', getTweets);
+  app.get('/trackings/new', newTrack);
 };
+
+function index (req, res) {
+  res.render('index');
+}
+
+function newTrack (req, res) {
+  res.render('track/new');
+}
 
 function getTweets (req, res) {
   var q = req.query;
 
   // Render page
-  res.render('index', {
+  res.render('search', {
     qPlayer: q.player,
     qTeam: q.team,
     qAuthor: q.author
